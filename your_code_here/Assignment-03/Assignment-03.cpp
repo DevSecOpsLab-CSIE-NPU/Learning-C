79ff63c0490420ef86f63e0aa733b939bec13be2
furuyuki-05 <abc940549@gmail.com>
2024-11-05 21:13:23 +0800

    Assignment-03

---
+++ your_code_here/Assignment-03/Assignment-03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string first, last;
+
+    cout << "Enter your first name: ";
+    cin >> first;
+    cout << "Enter your last name: ";
+    cin >> last;
+
+    cout << "Hello " << first << " " << last << "!" << endl;
+    return 0;
+}
