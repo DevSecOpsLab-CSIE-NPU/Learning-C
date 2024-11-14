58b1b15cec3001c17cbd0a5bfaded70938aa19a5
黃鴻霖 <d11405067@ems.npu.edu.tw>
2024-11-14 21:40:05 +0800

    Create Assignment-09.c

---
+++ your_code_here/Assignment-09.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3 || argc > 3)
+    {
+        printf("Usage: ./Assignment-09 Fristname Lastname");
+    }
+    else
+    {
+        printf("Hello, %s %s", argv[1], argv[2]);
+    }
+    return 0;
+}
